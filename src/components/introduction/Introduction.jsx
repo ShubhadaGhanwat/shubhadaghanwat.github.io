cdfba9e08d867ff058a50fea0394cc0061275edd
Shubhada  Ghanwat <shubhada.ghanwat7751@gmail.com>
2026-09-09 14:52:55 +0530

    Update Introduction.jsx

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -32,7 +32,7 @@ const Introduction = () => {
           <p className="text-3xl xxs:text-4xl sm:max-xl:text-5xl xl:text-6xl font-semibold w-full">
             Hello, I’m
             <span className="text-nowrap shrink-0 inline-block w-full">
-              Your Name
+              Shubhada Ghanwat
             </span>
           </p>
           <p className="text-xs xxs:text-lg lg:text-[18px] my-6">
